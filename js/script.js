985d842bd1d547eb92345f8a767c6d2df8ac9abf
antocrne <69148589+antocrne@users.noreply.github.com>
2021-01-20 18:05:17 +0100

    First commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper('.slider__index', {
+    direction: 'vertical',
+    slidesPerView: 1,
+    spaceBetween: 30,
+    speed: 800,
+    loop: true,
+    mousewheel: true,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'custom',
+        renderCustom: function (swiper, current, total) {
+        /*return ('0' + current).slice(-2) + ' <span class="slide__bar"></span> ' + ('0' + total).slice(-2);*/
+            return '<span class="current">'+ ('0' + current).slice(-2) +'</span>' +
+                    ' <span class="slide__bar"></span> ' +
+                    '<span class="total">'+ ('0' + total).slice(-2) +'</span>';
+        }
+    },
+});
