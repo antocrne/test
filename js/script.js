159057e6ba252609bba8eb4b198090e2c9d103de
antocrne <69148589+antocrne@users.noreply.github.com>
2021-01-22 00:05:59 +0100

    new page and slider and animation page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+$(".appear").ready(function() {
+    
+    $(".js-reveal").animate({
+        opacity: 1,
+      }, 1000, function() {
+        // Animation complete.
+      });
+})
+
 $('#toggle').click(function() {
     $(this).toggleClass('active');
     $(".menu__overlay").toggleClass('visible');
@@ -24,6 +33,25 @@ var swiper = new Swiper('.slider__index', {
             return '<span class="current">'+ ('0' + current).slice(-2) +'</span>' +
                     ' <span class="slide__bar"></span> ' +
                     '<span class="total">'+ ('0' + total).slice(-2) +'</span>';
+        }
+    },
+});
+
+var swiper = new Swiper('.swiper__content', {
+    direction: 'vertical',
+    slidesPerView: 1,
+    spaceBetween: 30,
+    speed: 800,
+    loop: true,
+    mousewheel: true,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'custom',
+        renderCustom: function (swiper, current, total) {
+        /*return ('0' + current).slice(-2) + ' <span class="slide__bar"></span> ' + ('0' + total).slice(-2);*/
+            return '<span class="current__c">'+'<span class="current">'+ ('0' + current).slice(-2) +'</span>'+'</span>' +
+                    ' <span class="slide__bar"></span> ' +
+                    '<span class="total__c">'+'<span class="total">'+ ('0' + total).slice(-2) +'</span>'+'</span>';
         }
     },
 });
